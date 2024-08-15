f60b7e0580af82c43333eaac9d91270e4f3bf618
unknown <wassimomari01@gmail.com>
2024-08-15 20:38:03 +0100

    pass 2st stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -38,7 +38,7 @@ func main() {
 }
 
 func matchLine(line []byte, pattern string) (bool, error) {
-	if utf8.RuneCountInString(pattern) != 1 {
+	if utf8.RuneCountInString(pattern) == 0 {
 		return false, fmt.Errorf("unsupported pattern: %q", pattern)
 	}
 	var ok bool
@@ -47,7 +47,6 @@ func matchLine(line []byte, pattern string) (bool, error) {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this to pass the first stage
-	ok = bytes.ContainsAny(line, pattern)
 	if pattern == "\\d" {
 		ok = bytes.ContainsAny(line, "0123456789")
 	} else {
