3a7dbb13866113ecb9b57d8bb7710aa989fb8924
unknown <wassimomari01@gmail.com>
2024-08-17 17:46:38 +0100

    pass 1st stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -67,7 +67,7 @@ func matchLine(line []byte, pattern string) (bool, error) {
 		if found && before == "" {
 			after1, found1 := strings.CutSuffix(after, "]")
 			if found1 {
-				ok = bytes.ContainsAny(line, after1)
+				ok = !bytes.ContainsAny(line, after1)
 			}
 		}
 	} else {
