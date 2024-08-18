f20afed0e3af6de2e85d058b8aa5a5899ed5e5b3
unknown <wassimomari01@gmail.com>
2024-08-18 20:37:21 +0100

    pass 1st stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	// Uncomment this to pass the first stage
-	"bytes"
+
 	"fmt"
 	"io"
 	"os"
-	"strings"
-	"unicode/utf8"
+	"unicode"
 )
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
@@ -25,7 +24,7 @@ func main() {
 		os.Exit(2)
 	}
 
-	ok, err := matchLine(line, pattern)
+	ok, err := matchLine(string(line), pattern)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(2)
@@ -38,78 +37,103 @@ func main() {
 	// default exit code is 0 which means success
 }
 
-func matchLine(line []byte, pattern string) (bool, error) {
-	if utf8.RuneCountInString(pattern) == 0 {
-		return false, fmt.Errorf("unsupported pattern: %q", pattern)
+func matchLine(line string, pattern string) (bool, error) {
+	// if utf8.RuneCountInString(pattern) == 0 {
+	// 	return false, fmt.Errorf("unsupported pattern: %q", pattern)
+	// }
+	// var ok bool
+	for i := 0; i <= len(line); i++ {
+		if matchPattern(line, pattern, i) {
+			return true, nil
+		}
+
 	}
-	var ok bool
+	return false, nil
+	// // You can use print statements as follows for debugging, they'll be visible when running tests.
+	// fmt.Println("Logs from your program will appear here!")
+	// fmt.Println(pattern)
+	// wordPattern := strings.ReplaceAll(pattern, "\\d", "")
+	// wordPattern1 := strings.ReplaceAll(wordPattern, "\\w", "")
+	// wordPattern1 = strings.TrimSpace(wordPattern1)
+	// subslice := []byte(wordPattern1)
+	// fmt.Println(wordPattern1)
+	// fmt.Println("IF IT CONTAINS THE WORD PATTERN: ", bytes.Contains(line, subslice))
+	// fmt.Println("IF IT CONTAINS THE LETTER PATTERN: ", bytes.ContainsAny(line, wordPattern1))
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-	fmt.Println(pattern)
-	wordPattern := strings.ReplaceAll(pattern, "\\d", "")
-	wordPattern1 := strings.ReplaceAll(wordPattern, "\\w", "")
-	wordPattern1 = strings.TrimSpace(wordPattern1)
-	subslice := []byte(wordPattern1)
-	fmt.Println(wordPattern1)
-	fmt.Println("IF IT CONTAINS THE WORD PATTERN: ", bytes.Contains(line, subslice))
-	fmt.Println("IF IT CONTAINS THE LETTER PATTERN: ", bytes.ContainsAny(line, wordPattern1))
+	// // dPattern : string of \d
+	// dPattern := strings.Trim(pattern, wordPattern1)
+	// dPattern = strings.Trim(dPattern, "\\w")
+	// dPattern = strings.TrimSpace(dPattern)
 
-	// dPattern : string of \d
-	dPattern := strings.Trim(pattern, wordPattern1)
-	dPattern = strings.Trim(dPattern, "\\w")
-	dPattern = strings.TrimSpace(dPattern)
+	// // counterD of \d
+	// counterD := strings.Count(pattern, "\\d")
+	// fmt.Println("counterD :", counterD)
+	// fmt.Println("dPattern length :", len(dPattern))
 
-	// counterD of \d
-	counterD := strings.Count(pattern, "\\d")
-	fmt.Println("counterD :", counterD)
-	fmt.Println("dPattern length :", len(dPattern))
+	// // wPattern : string of \w
+	// wPattern := strings.Trim(pattern, wordPattern1)
+	// wPattern = strings.Trim(wPattern, "\\d")
+	// wPattern = strings.TrimSpace(wPattern)
+	// // counterW of \d
+	// counterW := strings.Count(pattern, "\\w")
+	// fmt.Println("counterW :", counterW)
+	// fmt.Println("wPattern length and wPattern:", len(wPattern), wPattern)
 
-	// wPattern : string of \w
-	wPattern := strings.Trim(pattern, wordPattern1)
-	wPattern = strings.Trim(wPattern, "\\d")
-	wPattern = strings.TrimSpace(wPattern)
-	// counterW of \d
-	counterW := strings.Count(pattern, "\\w")
-	fmt.Println("counterW :", counterW)
-	fmt.Println("wPattern length and wPattern:", len(wPattern), wPattern)
+	// fmt.Println("subslice length :", len(subslice))
 
-	fmt.Println("subslice length :", len(subslice))
-
-	if strings.Contains(pattern, "\\d") {
-		ok1 := (counterD*2 - 1) == len(dPattern)
-		if ok1 {
-			ok = bytes.Contains(line, subslice)
+	// if strings.Contains(pattern, "\\d") {
+	// 	ok1 := (counterD*2 - 1) == len(dPattern)
+	// 	if ok1 {
+	// 		ok = bytes.Contains(line, subslice)
+	// 	}
+	// } else if strings.Contains(pattern, "\\w") {
+	// 	ok1 := (counterW*2 - 1) == len(wPattern)
+	// 	// ok1 := bytes.ContainsAny(line, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_")
+	// 	if ok1 && len(subslice) > 1 {
+	// 		ok = bytes.Contains(line, subslice)
+	// 	}
+	// } else if isPosCharacterGroup(pattern) == "positive" {
+	// 	//pattern : [abc] : matches every letter in the brackers
+	// 	after, found := strings.CutPrefix(pattern, "[")
+	// 	if found {
+	// 		after1, found1 := strings.CutSuffix(after, "]")
+	// 		if found1 {
+	// 			ok = bytes.ContainsAny(line, after1)
+	// 		}
+	// 	}
+	// } else if isPosCharacterGroup(pattern) == "negative" {
+	// 	//pattern : [^abc] : matches every letter except the ones in the brackers
+	// 	before, after, found := strings.Cut(pattern, "[^")
+	// 	if found && before == "" {
+	// 		after1, found1 := strings.CutSuffix(after, "]")
+	// 		if found1 {
+	// 			ok = !bytes.ContainsAny(line, after1)
+	// 		}
+	// 	}
+	// } else {
+	// 	ok = bytes.ContainsAny(line, pattern)
+	// }
+	// fmt.Println("LAST OK :", ok, nil)
+	// return ok, nil
+}
+func matchPattern(line string, pattern string, pos int) bool {
+	n := len(pattern)
+	j := pos
+	for i := 0; i < n; i++ {
+		if j >= len(line) {
+			return false
 		}
-	} else if strings.Contains(pattern, "\\w") {
-		ok1 := (counterW*2 - 1) == len(wPattern)
-		// ok1 := bytes.ContainsAny(line, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_")
-		if ok1 && len(subslice) > 1 {
-			ok = bytes.Contains(line, subslice)
-		}
-	} else if isPosCharacterGroup(pattern) == "positive" {
-		//pattern : [abc] : matches every letter in the brackers
-		after, found := strings.CutPrefix(pattern, "[")
-		if found {
-			after1, found1 := strings.CutSuffix(after, "]")
-			if found1 {
-				ok = bytes.ContainsAny(line, after1)
+		if pattern[i] == '\\' && i+1 < n {
+			if pattern[i+1] == 'd' && !unicode.IsDigit(rune(line[j])) {
+				return false
+			} else if pattern[i+1] == 'w' && !(unicode.IsLetter(rune(line[j])) || unicode.IsDigit(rune(line[j])) || line[j] == '_') {
+				return false
+			} else {
+				i++
 			}
 		}
-	} else if isPosCharacterGroup(pattern) == "negative" {
-		//pattern : [^abc] : matches every letter except the ones in the brackers
-		before, after, found := strings.Cut(pattern, "[^")
-		if found && before == "" {
-			after1, found1 := strings.CutSuffix(after, "]")
-			if found1 {
-				ok = !bytes.ContainsAny(line, after1)
-			}
-		}
-	} else {
-		ok = bytes.ContainsAny(line, pattern)
 	}
-	fmt.Println("LAST OK :", ok, nil)
-	return ok, nil
+	return true
 }
 
 func isPosCharacterGroup(pattern string) string {
