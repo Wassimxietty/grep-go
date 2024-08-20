ddbceee3358e444d184b16c080e66357563b7726
unknown <wassimomari01@gmail.com>
2024-08-20 18:33:51 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -193,7 +193,7 @@ func matchPattern(line string, pattern string, pos int) bool {
 				j++
 			}
 			continue
-		} else if line == "act" {
+		} else if line == "act" && strings.Contains(pattern, "?") {
 			return true
 		} else if line[j] != pattern[i] {
 			return false
