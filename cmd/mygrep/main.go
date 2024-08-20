9d991b6bc641b6ae08ee7013894f4699d000033b
unknown <wassimomari01@gmail.com>
2024-08-20 22:16:29 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -218,10 +218,11 @@ func matchPattern(line string, pattern string, pos int) bool {
 		} else if strings.Contains(pattern, "?") && line == "act" {
 			return true
 		} else if pattern[i] == '(' {
+			i++
 			endPos := strings.Index(pattern[i:], "|")
 			matchAnyPattern := pattern[i:endPos]
 			for i := 0; i < endPos; i++ {
-				if matchAnyPattern[i] != line[j] {
+				if rune(matchAnyPattern[i]) != rune(line[j]) {
 					return false
 				}
 				j++
