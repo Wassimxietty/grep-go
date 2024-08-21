3d4e1a26a60b80c0c5a9ad6e9d0afac3d8cde77b
unknown <wassimomari01@gmail.com>
2024-08-21 23:45:46 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -175,6 +175,9 @@ func matchPattern(line string, pattern string, pos int) bool {
 				if unicode.IsDigit(rune(pattern[i+1])) {
 					number := int(pattern[i+1]-'0') - 1
 					patternMatch := patternArray[number]
+					if patternMatch[0] == '(' && patternMatch[len(patternMatch)-1] == ')' {
+						patternMatch = patternMatch[1:]
+					}
 					if !matchPattern(line, patternMatch, j) {
 						return false
 					}
