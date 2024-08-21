ec15ffc642bb1ed5709e2ba2a2c9de7afebe74db
unknown <wassimomari01@gmail.com>
2024-08-21 21:12:33 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -243,7 +243,7 @@ func matchPattern(line string, pattern string, pos int) bool {
 			if endIndex == -1 || i >= index {
 				return false
 			}
-			if matchPattern(line, pattern[i:index], 0) {
+			if matchPattern(line, pattern[i:index], j) {
 				i = endIndex
 				continue
 			}
