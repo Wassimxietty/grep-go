8db5af6f37e0eb4af681d0884e4f9ba6d703dad2
unknown <wassimomari01@gmail.com>
2024-08-21 23:49:38 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -75,12 +75,10 @@ func matchLine(line string, pattern string) (bool, error) {
 	// 	fmt.Println("firstWord : ", firstWord)
 
 	// }
-	// patternArray := strings.Split(pattern, " ")
-	// number := int(pattern[11]-'0') - 1
-	// patternMatch := patternArray[number]
-	// patternMatch = patternMatch[1 : len(patternMatch)-1]
-	// fmt.Println("number:  ", number)
-	// fmt.Println("patternMatch: ", patternMatch)
+	patternArray := strings.Split(pattern, " ")
+	patternMatch := patternArray[1]
+	patternMatch = patternMatch[1 : len(patternMatch)-1]
+	fmt.Println("patternMatch: ", patternMatch)
 	for i := 0; i <= len(line); i++ {
 		if matchPattern(line, pattern, i) {
 			return true, nil
