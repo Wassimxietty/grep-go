7cb552aee356fcd1df6836ae41a91cc4af327ae4
unknown <wassimomari01@gmail.com>
2024-08-21 17:26:31 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -161,6 +161,7 @@ func matchPattern(line string, pattern string, pos int) bool {
 			return false
 		}
 		if pattern[i] == '\\' && i+1 < n {
+			return true
 			switch pattern[i+1] {
 			case 'd':
 				if !unicode.IsDigit(rune(line[j])) {
