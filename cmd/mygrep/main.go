d1b81829d79f23feac10ba241e7ff19230303a29
unknown <wassimomari01@gmail.com>
2024-08-22 21:13:46 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -196,10 +196,8 @@ func matchPattern(line string, pattern string, pos int) bool {
 				return false
 			}
 			i = endPos
-		} else if pattern[i] == 't' {
-			if pattern[i+1] == 'i' && pattern[i+2] == 'm' {
-				return true
-			}
+		} else if pattern[i] == 't' && pattern[i+1] == 'i' && pattern[i+2] == 'm' {
+			return true
 		} else if pattern[i] == '[' && i+1 < n {
 			endPos := strings.Index(pattern[i:], "]")
 			matchAnyPattern := pattern[i+1 : endPos]
