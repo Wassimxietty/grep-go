510113b610d7bc821b9e6c8e7d41d3e35b839568
unknown <wassimomari01@gmail.com>
2024-08-24 13:40:10 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -110,7 +110,7 @@ func matchPattern(line string, pattern string, pos int) bool {
 					return false
 				}
 			case 'w':
-				fmt.Println("rani hna: ", !(unicode.IsLetter(rune(line[j])) || unicode.IsDigit(rune(line[j])) || line[j] == '_'))
+				fmt.Println("rani hna: ", (unicode.IsLetter(rune(line[j])) || unicode.IsDigit(rune(line[j])) || line[j] == '_'))
 				if !(unicode.IsLetter(rune(line[j])) || unicode.IsDigit(rune(line[j])) || line[j] == '_') {
 					return false
 				}
