18944da0698c4a585b97389e54c026b90a3edfe8
unknown <wassimomari01@gmail.com>
2024-08-25 18:44:57 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -185,7 +185,7 @@ func matchPattern(line string, pattern string, pos int) (bool, int) {
 					j++
 				}
 				//it's somehow putting my j with a plus 2?
-				j -= 2
+				j -= 1
 			} else if !strings.ContainsAny(matchAnyPattern, string(line[j])) {
 				return false, j
 			}
