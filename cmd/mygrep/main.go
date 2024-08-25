826ea903db3fb55fc8b3301c545a8b9886ba1b43
unknown <wassimomari01@gmail.com>
2024-08-25 18:21:09 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -166,7 +166,7 @@ func matchPattern(line string, pattern string, pos int) (bool, int) {
 			} else if strings.ContainsAny(matchAnyPattern, string(line[j])) {
 				return false, j
 			}
-			i = endPos + 1
+			i = endPos
 			fmt.Println("i: ", i)
 
 		} else if pattern[i] == '[' && i+1 < n {
