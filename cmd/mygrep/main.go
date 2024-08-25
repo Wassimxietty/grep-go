bd6d12e11eab3f17769e4179f178686b5c21130b
unknown <wassimomari01@gmail.com>
2024-08-25 17:35:15 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -165,6 +165,7 @@ func matchPattern(line string, pattern string, pos int) (bool, int) {
 			//goodluck, tomorrow me
 			endPos := strings.Index(pattern[i:], "]")
 			matchAnyPattern := pattern[i+1 : endPos]
+			fmt.Println("matchAnyPattern: ", string(matchAnyPattern))
 
 			if pattern[i+1] == '^' {
 				fmt.Println("^ was found ")
