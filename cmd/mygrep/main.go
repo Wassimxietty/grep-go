a7bbb9397530f6ff5578b57fe44c9fc6dd3ac3c1
unknown <wassimomari01@gmail.com>
2024-08-25 23:04:33 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -281,6 +281,7 @@ func matchPattern(line string, pattern string, pos int) (bool, int) {
 				return false, j
 			}
 			okay, jj := matchPattern(line, pattern[i:index], 0)
+			fmt.Println("jj: ", jj)
 			if !okay {
 				return false, jj
 			} else {
