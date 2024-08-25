2614882f7f760acb05db4483973feb50268f468b
unknown <wassimomari01@gmail.com>
2024-08-25 21:38:50 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -206,6 +206,9 @@ func matchPattern(line string, pattern string, pos int) (bool, int) {
 					for endPos < n && pattern[endPos] != ')' {
 						endPos++
 					}
+					lineSplit := strings.Split(line, " ")
+					fmt.Println("lineSplit: ", lineSplit)
+
 					patternMatch := pattern[i+1 : endPos+1]
 					fmt.Println("patternMatch: ", patternMatch)
 					okay, jj := matchPattern(line, patternMatch, 0)
