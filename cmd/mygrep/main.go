8c18e94e7907c09a4109ac504d66f33ac72084d8
unknown <wassimomari01@gmail.com>
2024-08-25 16:59:41 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -164,8 +164,8 @@ func matchPattern(line string, pattern string, pos int) (bool, int) {
 			//goodluck, tomorrow me
 			endPos := strings.Index(pattern[i:], "]")
 			matchAnyPattern := pattern[i+1 : endPos]
-			fmt.Print(line[j])
-			fmt.Print(pattern[i])
+			fmt.Print(string(line[j]))
+			fmt.Print(string(pattern[i]))
 			if pattern[i+1] == '^' {
 				fmt.Println("^ was found ")
 			}
