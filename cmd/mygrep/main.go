74f2b1c89de01e7027b29891035cca1fed5b1a2a
unknown <wassimomari01@gmail.com>
2024-08-28 00:30:45 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -327,7 +327,7 @@ func matchPattern(line string, pattern string, pos int) (bool, int) {
 				}
 			}
 			//handling lineArray
-			word := line[j : jj-1]
+			word := line[j:jj]
 			lineArray = append(lineArray, word)
 			fmt.Println("lineArray: ", lineArray)
 			i = endIndex
