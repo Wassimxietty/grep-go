f657ea588225075a2b1dadb896b78f06498a5316
unknown <wassimomari01@gmail.com>
2024-08-27 22:24:18 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -294,7 +294,7 @@ func matchPattern(line string, pattern string, pos int) (bool, int) {
 			}
 			j--
 		} else if pattern[i] == '(' {
-			endIndex := strings.Index(pattern[i:], ")")
+			endIndex := strings.Index(pattern[i:], ")") + i
 			index := strings.Index(pattern, "|")
 			i++
 			if index == -1 {
