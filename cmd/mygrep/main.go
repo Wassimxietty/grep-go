d3f8b9f8bf9c090dab775e6262e75eff87be257d
unknown <wassimomari01@gmail.com>
2024-08-28 02:14:59 +0100

    pass stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -302,16 +302,17 @@ func matchPattern(line string, pattern string, pos int) (bool, int) {
 			j--
 		} else if pattern[i] == '(' {
 			endIndex := strings.Index(pattern[i:], ")") + i
-			// index := strings.Index(pattern, "|")
-			index := i + 1
-			if strings.Contains(pattern[i:endIndex], "|") {
-				for index < n && pattern[index] != ')' {
-					index++
-				}
-			}
+			index := strings.Index(pattern, "|")
 			i++
 			if index == -1 {
 				index = endIndex
+			} else {
+				if strings.Contains(pattern[i:endIndex], "|") {
+					index := i + 1
+					for index < n && pattern[index] != ')' {
+						index++
+					}
+				}
 			}
 			if endIndex == -1 || i >= index {
 				fmt.Println("endIndex ?: ", endIndex, "i: ", i, "index: ", index)
