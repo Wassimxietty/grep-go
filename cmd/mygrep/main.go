2aeb9e9a79e7a999e772b786cb5afe2ae0be08d3
Wassimxietty <wassimomari01@gmail.com>
2024-08-29 18:47:51 +0100

    test

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -191,7 +191,7 @@ func matchPattern(line string, pattern string, pos int) (bool, int) {
 				if unicode.IsDigit(rune(pattern[i+1])) {
 
 					number := int(pattern[i+1]-'0') - 1
-					if number == -1 && number-1 < len(lineArray) {
+					if number == -1 || number-1 < len(lineArray) {
 						fmt.Println("patternArray[number] is patternArray[-1]")
 						return false, j
 					}
