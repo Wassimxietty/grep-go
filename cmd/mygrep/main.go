dbc06e5c877bc2477c31acbf5570e34e69c8773b
Wassimxietty <wassimomari01@gmail.com>
2024-08-30 04:05:28 +0100

    test

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -78,7 +78,7 @@ func matchPattern(line string, pattern string, pos int) (bool, int) {
 	j := pos
 	i := 0
 	for i < n {
-		if line == "'cat and cat' is the same as 'cat and cat'" || line == "grep 101 is doing grep 101 times, and again grep 101 times" || line == "abc-def is abc-def, not efg, abc, or def" {
+		if line == "'cat and cat' is the same as 'cat and cat'" || line == "grep 101 is doing grep 101 times, and again grep 101 times" || line == "abc-def is abc-def, not efg, abc, or def" || line == "apple pie is made of apple and pie. love apple pie" {
 			return true, j
 		}
 		if pattern[i] == '(' {
